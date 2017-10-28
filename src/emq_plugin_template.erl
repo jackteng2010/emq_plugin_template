f7819cb094df528eb37c38de378110084658373a
za-tengxianchao <tengxianchao@zhongan.io>
2017-10-28 20:03:31 +0800

    fix issue

--- src/emq_plugin_template.erl
+++ src/emq_plugin_template.erl
@@ -38,7 +38,7 @@ load(Env) ->
     emqttd:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]).
 
 on_client_connected(ConnAck, Client = #mqtt_client{client_id  = ClientId, username = Username}, Env) ->
-	lager:info("client ~s/~s connected, connack: ~w", [ClientId, Username, ConnAck]),
+	io:format("client ~s/~s connected, connack: ~w~n", [ClientId, Username, ConnAck]),
     Json = mochijson2:encode([{type, <<"connected">>},
 								{clientid, ClientId},
 								{username, Username},
@@ -47,7 +47,7 @@ on_client_connected(ConnAck, Client = #mqtt_client{client_id  = ClientId, userna
     {ok, Client}.
 
 on_client_disconnected(Reason, _Client = #mqtt_client{client_id = ClientId, username = Username}, Env) ->
-    lager:info("client  ~s/~s disconnected, reason: ~w", [ClientId, Username, Reason]),
+    io:format("client  ~s/~s disconnected, reason: ~w~n", [ClientId, Username, Reason]),
     Json = mochijson2:encode([{type, <<"disconnected">>},
 								{clientid, ClientId},
 								{username, Username},
@@ -57,7 +57,7 @@ on_client_disconnected(Reason, _Client = #mqtt_client{client_id = ClientId, user
     ok.
 
 on_session_created(ClientId, Username, _Env) ->
-	lager:info("session(~s/~s) created.", [ClientId, Username]),
+	io:format("session(~s/~s) created.~n", [ClientId, Username]),
     Json = mochijson2:encode([{type, <<"session_created">>},
 								{clientid, ClientId},
 								{username, Username},
@@ -65,7 +65,7 @@ on_session_created(ClientId, Username, _Env) ->
 	produce_to_kafka(Json).
 
 on_session_terminated(ClientId, Username, Reason, _Env) ->
-	lager:info("session(~s/~s) terminated: ~p.", [ClientId, Username, Reason]),
+	io:format("session(~s/~s) terminated: ~p.~n", [ClientId, Username, Reason]),
     Json = mochijson2:encode([{type, <<"session_terminated">>},
 								{clientid, ClientId},
 								{username, Username},
@@ -79,7 +79,7 @@ on_message_publish(Message = #mqtt_message{from = {ClientId, Username},
                         dup     = Dup,
                         topic   = Topic,
                         payload = Payload}, _Env) ->
-    lager:info("publish >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"),
+    io:format("publish >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>~n"),
     Json = mochijson2:encode([{type, <<"publish">>},
 								{clientid, ClientId},
 								{username, Username},
@@ -100,7 +100,7 @@ ekaf_init(_Env) ->
 	application:set_env(ekaf, ekaf_bootstrap_topics, BootstrapTopic),
     application:set_env(ekaf, ekaf_bootstrap_broker, BootstrapBroker),
 	{ok, _} = application:ensure_all_started(ekaf),
-    lager:info("Init ekaf with ~p ~p", [BootstrapBroker, BootstrapTopic]).
+    io:format("Init ekaf with ~p ~p~n", [BootstrapBroker, BootstrapTopic]).
 	
 %% Called when the plugin application stop
 unload() ->
@@ -115,7 +115,7 @@ produce_to_kafka(Json) ->
 	{ok, KafkaValue} = application:get_env(emq_plugin_template, kafka),
 	BootstrapTopic = proplists:get_value(bootstrap_topic, KafkaValue),
 	
-	lager:info("produce to kafka json ~p", [Json]),
+	io:format("produce to kafka json ~p~n", [Json]),
 %%     try ekaf:produce_sync(BootstrapTopic, <<"foo 123">>)
 %%     catch
 %%         error:exists -> lager:error("produce_sync error")
@@ -123,14 +123,14 @@ produce_to_kafka(Json) ->
 	
 %% 	ekaf:produce_sync(<<"tech-iot-device-gateway-2040">>, <<"foo 123">>),
 	
-%% 	lager:info("produce to kafka 222 ~p ~n", [BootstrapTopic]),
+%% 	io:format("produce to kafka 222 ~p ~n", [BootstrapTopic]),
 %% 	ekaf:produce_sync(<<"tech-iot-device-gateway-2040">>, list_to_binary(Json)),
 	
-%% 	lager:info("produce to kafka 333 ~p ~n", [BootstrapTopic]),
+%% 	io:format("produce to kafka 333 ~p ~n", [BootstrapTopic]),
 %% 	Re = ekaf:produce_async(<<BootstrapTopic>>, list_to_binary(Json)),
 	
-%% 	lager:info("Kafka response ~s~n", [Re]).
-	lager:info("Kafka response").
+%% 	io:format("Kafka response ~s~n", [Re]).
+	io:format("Kafka response ~n").
 
 
 
